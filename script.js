959ad0ac1bd5024fb6ae1f42ea0f8862837017ed
Thiago Souza <thiagosouza1421@hotmail.com>
2019-10-29 06:00:50 -0200

    Adionando Indicadores,Cores e texto.

--- script.js
+++ script.js
@@ -1,37 +1,74 @@
+//Definindo Cores
+let corHora = (56, 72, 131);
+let corMinuto = (228, 93, 80);
+let corSegundo = (185, 239, 163);
+let corBackground = (255, 241, 241);
+
 function setup() {
-    //Criando Janela Canvas
-    createCanvas(400,400);
+    //Criando Canvas
+    createCanvas(800,800);
     //Definindo medida 
     angleMode(DEGREES);
   }
   
   function draw() {
+   background(corBackground);
    //Setando Horário Local.
    let horas = hour();
    let minutos = minute();
    let segundos = second();
    
-   background(0);
-   translate(200, 200);
+   
+   translate(400, 400);
    rotate(-90);
-   strokeWeight(8);
    noFill();
    
    //Definindo Arco Hora.
    stroke(255, 100, 255);
    let rotacaoHoras = map(horas % 12, 0, 12, -90, 360);
-   arc(0, 0, 260, 260, 0, rotacaoHoras);
+   strokeWeight(21);
+   arc(0, 0, 520, 520, 0, rotacaoHoras);
+
+   push();
+   rotate(rotacaoHoras);
+   strokeWeight(8);
+   stroke(corHora);
+   line(0, 0, 60, 0);
+   pop();
 
    //Definindo Arco Minutos.
    stroke(255, 100, 150);
    let rotacaoMinutos = map(minutos, 0, 60, 0, 360);
-   arc(0, 0, 280, 280, 0, rotacaoMinutos);
+   strokeWeight(21);
+   arc(0, 0, 560, 560, 0, rotacaoMinutos);
 
+   push();
+   rotate(rotacaoMinutos);
+   strokeWeight(4);
+   stroke(corMinuto);
+   line(0, 0, 100, 0);
+   pop();
+   
    //Definindo Arco Segundos.
    stroke(100, 255, 100);
    let rotacaoSegundos = map(segundos, 0, 60, 0, 360);
-   arc(0, 0, 300, 300, 0, rotacaoSegundos);
+   strokeWeight(21);
+   arc(0, 0, 600, 600, 0, rotacaoSegundos);
+
+   push();
+   rotate(rotacaoSegundos);
+   strokeWeight(2);
+   stroke(corSegundo);
+   line(0, 0, 120, 0);
+   pop();
 
    //Horário Local
-   //text(horas + ':' + minutos + ':' + segundos,10,200);
-  }
+   noStroke();
+   rotate(90);
+   fill(0);
+   textSize(50);
+   text(horas + ':' + minutos + ':' + segundos,-80,200);
+
+   
+ 
+}
